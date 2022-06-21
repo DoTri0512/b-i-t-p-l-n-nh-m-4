e4398f7cc76b4356dbb7ce5acf591604aa35aea3
2051063936-3011 <nbtung772@gmail.com>
2022-06-22 00:03:44 +0700

    about

---
+++ b-i-t-p-l-n-nh-m-4-main/js/index1.js
@@ -0,0 +1,8 @@
+function openFunction(){
+    var nav_mobile = document.getElementById("header__mobile-menu")
+    var overlay = document.getElementById("header__menu-overlay")
+
+    nav_mobile.classList.toggle('active')
+    overlay.classList.toggle('active')
+
+ }
