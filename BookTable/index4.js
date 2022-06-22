a474a11b53995686c24c439ed1f00be6264282fe
Duong <tienca0610@gmail.com>
2022-06-22 21:10:56 +0700

    hoanthanh

--- BookTable/index4.js
+++ BookTable/index4.js
@@ -1,15 +1,21 @@
 function openFunction(){
-    var x = document.getElementById("header__mobile-menu")
-    var y = document.getElementById("header__menu-overlay")
-    if(y.style.display==="block" && x.style.display==="block"){
-        y.style.display="none";
-        x.style.display="none";
-    }
-    else{
-        y.style.display="block";
-        x.style.display="block";
-    }
+  var nav_mobile = document.getElementById("header__mobile-menu")
+  var overlay = document.getElementById("header__menu-overlay")
+
+  nav_mobile.classList.toggle('active')
+  overlay.classList.toggle('active')
+
 }
+AOS.init({
+  offset: 200,  //offset (in px) from the original trigger point
+  delay: 100, // values from 0 to 3000, with step 50ms
+  duration: 400,  //values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false,  //whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom',  //defines which position of the element regarding to window should trigger the animation
+});
+
 const MenuCloseEl = document.querySelector('.header__menu-bars');
 const MenuList = document.querySelector('.nav__list');
 const MenuIconEl = document.querySelector('.nav__icon');
